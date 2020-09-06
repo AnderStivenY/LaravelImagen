ad95ff10324aafc63af3d50b738de9d8c88424f0
Otoniel Hernández Garzón <hernandezottto@misena.edu.co>
2020-09-05 19:34:10 -0500

    Chore: Se modifica proyecto para un CRUD

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -1,7 +1,8 @@
 <?php
 
+use App\Users;
 use Illuminate\Database\Seeder;
-
+use Faker\Factory as Faker;
 class UsersTableSeeder extends Seeder
 {
     /**
@@ -11,6 +12,17 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $faker = Faker::create();
+        for($i=0; $i<50;$i++)
+        {$user = Users::create([
+            'names' => $faker -> name,
+            'lastnames' => $faker -> lastName,
+            'email' => $faker -> unique()-> email,
+            'profile_picture' => $faker -> image(__DIR__, 200, 200,null,true,true,$faker-> firstName.'.png'),
+            'gender' => $faker -> randomElement(['Male', 'Female']),
+            'birth_date' => $faker -> dateTime
+        ]);}
     }
 }
+
+
--- laragonMysql.session.sql
+++ laragonMysql.session.sql
@@ -1,15 +1,15 @@
-DROP DATABASE IF  EXISTS  laravel_imagen;
+DROP DATABASE IF EXISTS laravel_imagen;
 CREATE DATABASE laravel_imagen;
-
+DROP TABLE laravel_imagen.users,
 CREATE TABLE laravel_imagen.users(
-    id BIGINT(20) PRIMARY KEY auto_increment,
+    id BIGINT(20) PRIMARY KEY AUTO_INCREMENT,
     names varchar(50),
     lastnames varchar(50),
-    email varchar(100) unique,
-    profile_picture varchar(250),
-    gender ENUM ('Femenino','Maxculino'),
+    email varchar(100) UNIQUE,
+    profile_picture BLOB,
+    gender ENUM ('Male', 'Female'),
     birth_date date,
     created_at TIMESTAMP,
     updated_at TIMESTAMP
-
 )
+
